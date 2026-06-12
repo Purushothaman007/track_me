29c3d58083bc72268a77bf8c64e004db94f693a1
Purushothaman B <purushothamanbala10@gmail.com>
2026-06-12 19:52:22 +0530

    Fix UTCDateTime attribute error on date objects

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, Integer, String, Date, DateTime
 from sqlalchemy.types import TypeDecorator
 from database import Base
-from datetime import timezone
+from datetime import datetime, timezone
 
 class UTCDateTime(TypeDecorator):
     impl = DateTime
     cache_ok = True
 
     def process_bind_param(self, value, dialect):
-        if value is not None:
+        if value is not None and isinstance(value, datetime):
             if value.tzinfo is None:
                 value = value.replace(tzinfo=timezone.utc)
             else:
@@ -17,7 +17,7 @@ class UTCDateTime(TypeDecorator):
         return value
 
     def process_result_value(self, value, dialect):
-        if value is not None:
+        if value is not None and isinstance(value, datetime):
             if value.tzinfo is None:
                 value = value.replace(tzinfo=timezone.utc)
         return value
